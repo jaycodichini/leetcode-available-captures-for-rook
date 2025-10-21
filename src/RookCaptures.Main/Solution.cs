3d7efe9588529b92ca0534316ca25d06b7192a9f
jaycodichini <80603392+jaycodichini@users.noreply.github.com>
2025-10-21 15:25:13 -0500

    Implemented solution.

---
+++ src/RookCaptures.Main/Solution.cs
@@ -0,0 +1,126 @@
+﻿namespace RookCaptures.Main
+{
+    public class Solution
+    {
+        public int NumRookCaptures(char[][] board)
+        {
+            var rook = new ChessPiece(-1, -1);
+
+            var row = 0;
+            var column = 0;
+            while (row <= 7)
+            {
+                if (ChessPiece.IsRook(board[row][column]))
+                {
+                    rook = new ChessPiece(row, column);
+                    break;
+                }
+
+                column++;
+                if (column != 8)
+                    continue;
+
+                row++;
+                column = 0;
+            }
+
+            if (rook.Row == -1 || rook.Column == -1)
+                return 0;
+
+            return GetRowCaptures(board, rook) + GetColumnCaptures(board, rook);
+        }
+
+        private static int GetRowCaptures(char[][] board, ChessPiece rook)
+        {
+            var captures = 0;
+            if (rook.Column > 0)
+            {
+                for (var i = rook.Column - 1; i >= 0; i--)
+                {
+                    if (ChessPiece.IsBishop(board[rook.Row][i]))
+                        break;
+
+                    if (!ChessPiece.IsPawn(board[rook.Row][i]))
+                        continue;
+
+                    captures++;
+                    break;
+                }
+            }
+
+            if (rook.Column < 7)
+            {
+                for (var i = rook.Column + 1; i <= 7; i++)
+                {
+                    if (ChessPiece.IsBishop(board[rook.Row][i]))
+                        break;
+
+                    if (!ChessPiece.IsPawn(board[rook.Row][i]))
+                        continue;
+
+                    captures++;
+                    break;
+                }
+            }
+            
+            return captures;
+        }
+
+        private static int GetColumnCaptures(char[][] board, ChessPiece rook)
+        {
+            var captures = 0;
+            if (rook.Row > 0)
+            {
+                for (var i = rook.Row - 1; i >= 0; i--)
+                {
+                    if (ChessPiece.IsBishop(board[i][rook.Column]))
+                        break;
+
+                    if (!ChessPiece.IsPawn(board[i][rook.Column]))
+                        continue;
+
+                    captures++;
+                    break;
+                }
+            }
+
+            if (rook.Row < 7)
+            {
+                for (var i = rook.Row + 1; i <= 7; i++)
+                {
+                    if (ChessPiece.IsBishop(board[i][rook.Column]))
+                        break;
+
+                    if (!ChessPiece.IsPawn(board[i][rook.Column]))
+                        continue;
+
+                    captures++;
+                    break;
+                }
+            }
+
+            return captures;
+        }
+
+        private class ChessPiece(int row, int column)
+        {
+            public int Row { get; } = row;
+            public int Column { get; } = column;
+
+            public static bool IsBishop(char value)
+            {
+                return value == 'B';
+            }
+
+            public static bool IsPawn(char value)
+            {
+                return value == 'p';
+            }
+
+            public static bool IsRook(char value)
+            {
+                return value == 'R';
+            }
+        }
+    }
+}
---
+++ src/RookCaptures.Test/SolutionTests.cs
@@ -0,0 +1,78 @@
+﻿using RookCaptures.Main;
+
+namespace RookCaptures.Test
+{
+    [TestFixture]
+    internal class SolutionTests
+    {
+        private Solution _solution;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _solution = new Solution();
+        }
+
+        [Test]
+        [TestCaseSource(typeof(SolutionTestCases), nameof(SolutionTestCases.BoardCases))]
+        public int NumRookCaptures_ReturnsExpectedCaptures(char[][] board)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, board.Select(characters => string.Join(",", characters.Select(character => $"\"{character}\"")))));
+            return _solution.NumRookCaptures(board);
+        }
+
+        private static class SolutionTestCases
+        {
+            public static IEnumerable<TestCaseData> BoardCases
+            {
+                get
+                {
+                    object board = new char[][]
+                    {
+                        ['.', '.', '.', '.', '.', '.', '.', '.'],
+                        ['.', '.', '.', 'p', '.', '.', '.', '.'],
+                        ['.', '.', '.', 'R', '.', '.', '.', 'p'],
+                        ['.', '.', '.', '.', '.', '.', '.', '.'],
+                        ['.', '.', '.', '.', '.', '.', '.', '.'],
+                        ['.', '.', '.', 'p', '.', '.', '.', '.'],
+                        ['.', '.', '.', '.', '.', '.', '.', '.'],
+                        ['.', '.', '.', '.', '.', '.', '.', '.']
+                    };
+                    yield return new TestCaseData(board)
+                        .SetName("NumRookCaptures_WithPawnAboveAndBelowAndRight_ReturnsThreeCaptures")
+                        .Returns(3);
+
+                    board = new char[][]
+                    {
+                        ['.','.','.','.','.','.','.','.'],
+                        ['.','p','p','p','p','p','.','.'],
+                        ['.','p','p','B','p','p','.','.'],
+                        ['.','p','B','R','B','p','.','.'],
+                        ['.','p','p','B','p','p','.','.'],
+                        ['.','p','p','p','p','p','.','.'],
+                        ['.','.','.','.','.','.','.','.'],
+                        ['.','.','.','.','.','.','.','.']
+                    };
+                    yield return new TestCaseData(board)
+                        .SetName("NumRookCaptures_WithBishopLeftAndAboveAndRightAndBelow_ReturnsZeroCaptures")
+                        .Returns(0);
+
+                    board = new char[][]
+                    {
+                        ['.','.','.','.','.','.','.','.'],
+                        ['.','.','.','p','.','.','.','.'],
+                        ['.','.','.','p','.','.','.','.'],
+                        ['p','p','.','R','.','p','B','.'],
+                        ['.','.','.','.','.','.','.','.'],
+                        ['.','.','.','B','.','.','.','.'],
+                        ['.','.','.','p','.','.','.','.'],
+                        ['.','.','.','.','.','.','.','.']
+                    };
+                    yield return new TestCaseData(board)
+                        .SetName("NumRookCaptures_WithPawnLeftAndAboveAndRightAndBishopBelow_ReturnsThreeCaptures")
+                        .Returns(3);
+                }
+            }
+        }
+    }
+}
